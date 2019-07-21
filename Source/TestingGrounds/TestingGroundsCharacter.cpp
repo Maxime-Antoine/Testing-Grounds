1c0e383a5a99288095c4d7babf4d6296b7b639cc
ANTOINE Maxime <maxime.antoine2@gmail.com>
2019-07-21 17:50:33 +0800

    Upgrade to Unreal 4.22

--- Source/TestingGrounds/TestingGroundsCharacter.cpp
+++ Source/TestingGrounds/TestingGroundsCharacter.cpp
@@ -60,7 +60,7 @@ ATestingGroundsCharacter::ATestingGroundsCharacter()
 
 	// Create VR Controllers.
 	R_MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("R_MotionController"));
-	R_MotionController->Hand = EControllerHand::Right;
+	R_MotionController->Hand_DEPRECATED = EControllerHand::Right;
 	R_MotionController->SetupAttachment(RootComponent);
 	L_MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("L_MotionController"));
 	L_MotionController->SetupAttachment(RootComponent);
